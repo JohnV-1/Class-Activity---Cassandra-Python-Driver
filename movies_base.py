2c43e1cae5e2c228beb4b349052797223e8b696f
John <johnvilla.dv@gmail.com>
2026-09-13 18:37:04 -0600

    Creacion de las tablas: movie_by_title y movie_by_genre

--- movies_base.py
+++ movies_base.py
@@ -7,8 +7,28 @@ CREATE_KEYSPACE = """
 CREATE KEYSPACE IF NOT EXISTS movies
 WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1}
 """
-CREATE_TABLE_MOVIE_BY_TITLE = ""
-CREATE_TABLE_MOVIE_BY_GENRE = ""
+CREATE_TABLE_MOVIE_BY_TITLE = CREATE_TABLE = """
+CREATE TABLE IF NOT EXISTS movie_by_title (
+movie_id UUID,
+title TEXT,
+release_year INT,
+genre TEXT,
+rating FLOAT,
+director TEXT,
+PRIMARY KEY (title, release_year)
+)
+"""
+CREATE_TABLE_MOVIE_BY_GENRE = """
+CREATE TABLE IF NOT EXISTS movie_by_genre (
+movie_id UUID,
+title TEXT,
+release_year INT,
+genre TEXT,
+rating FLOAT,
+director TEXT,
+PRIMARY KEY (genre, rating,movie_id)
+)
+"""
 INSERT_MOVIE_TITLE = ""
 INSERT_MOVIE_GENRE = ""
 DELETE_MOVIE_TITLE = ""
